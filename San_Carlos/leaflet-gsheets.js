0da9ae06bff0e8b699085f38818b83c54de919fc
nzigler <38933562+nzigler@users.noreply.github.com>
2020-04-28 12:45:22 -0700

    pb4

--- San_Carlos/leaflet-gsheets.js
+++ San_Carlos/leaflet-gsheets.js
@@ -270,9 +270,7 @@ function addPoints(data) {
             "<TR><TD><b>Comments from CSG: </b></TD><TD>" + e.target.feature.properties.Comments_CSG + "</TD></TR>" +  
             "<TR><TD><b>Comments from City: </b></TD><TD>" + e.target.feature.properties.Comments_City + "</TD></TR>" +  
             "<TR><TD><b>File Path CSG: </b></TD><TD>" + e.target.feature.properties.File_Path + "</TD></TR>" + 
-            /*"<TR><TD><b>Associated Document: </b></TD><TD><a href='docs/https://nzigler.github.io/San_Carlos/photos/example_bioretention.jpg'>" + e.target.feature.properties.Doc1 + "</a></TD></TR>" + */
             "<TR><TD colspan='2'> <img src='photos/" + e.target.feature.properties.Photo1 + "' width='250'></TD></TR>" +
-            /*"<TR><TD colspan='2'> <img src=photos/" + e.target.feature.properties.Photo1 + " width='250'></TD></TR>" +*/  
             "</TABLE>";
           
         sidebar.open(panelID);
