21ca04ae9b7db6a1069ad56ae57571f208733b1e
nzigler <38933562+nzigler@users.noreply.github.com>
2020-04-28 11:44:51 -0700

    geojson updates again

--- San_Carlos/leaflet-gsheets.js
+++ San_Carlos/leaflet-gsheets.js
@@ -428,14 +428,12 @@ baseLayers = {
 };
 
 
-lyrCities = L.geoJSON.ajax('data/SMC_Cities.geojson', {style:{color:'yellow', fillOpacity:0}});// .addTo(mymap);
-lyrParcels = L.geoJSON.ajax('data/SMC_Parcels_San_Carlos.geojson', {style:{color:'white', fillOpacity:0, weight:1.5}}); //.addTo(mymap);
-lyrWatersheds = L.geoJSON.ajax('data/SMC_watersheds.geojson', {style:{color:'deepskyblue', fillOpacity:0}}); //.addTo(mymap);
+lyrCities = L.geoJSON.ajax('data/SMC_Cities_lines.geojson', {style:{color:'yellow', fillOpacity:0}});// .addTo(mymap);
+lyrParcels = L.geoJSON.ajax('data/SMC_Parcels_San_Carlos_lines_reduced.geojson', {style:{color:'white', fillOpacity:0, weight:1.5}}); //.addTo(mymap);
 lyrCreeks = L.geoJSON.ajax('data/SMC_Creeks.geojson', {style:{color:'aqua'}}).addTo(map);
 
 overlays = {
     "City Boundaries":lyrCities,
-    "Watersheds":lyrWatersheds,
     "Creeks":lyrCreeks,
     "Parcels":lyrParcels    
 };
