e109675c55a7e0efe1da22bc68d43738ca1edcfb
nzigler <38933562+nzigler@users.noreply.github.com>
2020-04-28 13:50:23 -0700

    null3

--- San_Carlos/leaflet-gsheets.js
+++ San_Carlos/leaflet-gsheets.js
@@ -271,7 +271,7 @@ function addPoints(data) {
             "<TR><TD><b>Comments from City: </b></TD><TD>" + e.target.feature.properties.Comments_City + "</TD></TR>" +  
             "<TR><TD><b>File Path CSG: </b></TD><TD>" + e.target.feature.properties.File_Path + "</TD></TR>" +             
             "<TR><TD><b>Project Document: </b></TD><TD><a href='docs/" + e.target.feature.properties.Doc1 + "' >" + e.target.feature.properties.Doc1 + "</a></TD></TR>" +
-            if (e.target.feature.properties.Photo1 != "" || e.target.feature.properties.Photo1 != null || e.target.feature.properties.Photo1 != " ") {
+            if (e.target.feature.properties.Photo1) {
                 "<TR><TD colspan='2'> <a href='photos/" + e.target.feature.properties.Photo1 + "' target='_blank'><img src='photos/" + e.target.feature.properties.Photo1 + "' width='320'></a></TD></TR>" +
             }
             "</TABLE>";
